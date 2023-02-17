60ab1d643ab1361f77ea15f7d5f322b04777d828
Vika Shcherbak <vikashcherbaka@gmail.com>
2023-02-17 20:37:22 +0100

    transported

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+import Preview from "./Preview";
+import horseImg from "./horse.png"
 
 function App() {
+
+  const [text, setText] = useState(`# Greetings and salutations!
+## Welcome to the <mark>Markdown Previewer</mark>
+
+where you can ***preview your markdown*** in all its glory!
+
+you can add some <kbd>code</kbd>:
+
+\`console.log("Hello, world!")\`
+  
+\`\`\`
+function greeting(name) {
+  return "Hello, " + name + "!";
+}
+\`\`\`
+
+- you can list some item
+- and another item
+  1. and one more
+  2. and the last one
+
+> You can write blockquotes like this
+>> and even like this!
+    
+![Image](${horseImg})
+
+- [x] now just have a look at this cool horse
+- [ ] and now try to write some markdown yourself
+
+[made by uuuuuvika](https://github.com/uuuuuvika)
+`);
+
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="working-area-box">
+        <textarea value={text} onChange={handleChange} id="editor"></textarea>
+      </div>
+      <div className="working-area-box">
+        <Preview text={text} />
+      </div>
     </div>
   );
 }
---
+++ src/Preview.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { marked } from "marked";
+//to purify the input
+//https://github.com/kkomelin/isomorphic-dompurify
+//import DOMPurify from 'isomorphic-dompurify';
+
+function Preview({ text }) {
+    // in case i want to satitize 
+    // const clean = DOMPurify.sanitize(text);
+     const renderer = new marked.Renderer();
+     renderer.blockquote = (quote) =>
+        `<blockquote class="preview-blockquote">${quote}</blockquote>`;
+    // const parsedText = marked(clean, { breaks: true, renderer });
+    const parsedText = marked(text, {breaks: true, renderer});
+
+    return (
+        <div id="preview" dangerouslySetInnerHTML={{ __html: parsedText }}></div>
+    );
+}
+
+export default Preview;
